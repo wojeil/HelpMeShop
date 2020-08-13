7e471586aaa065a99f97dc151be5ff1b73fc13ec
Wadih Ojeil <wadih.ojeil@gmail.com>
2020-08-13 17:55:37 -0400

    added components such as header recipe recipelist recipe details and recipe item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,26 @@ import { NgModule } from '@angular/core';
 
 // import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HeaderComponent} from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent
+
   ],
   imports: [
     BrowserModule,
+    
    
   ],
   providers: [],
