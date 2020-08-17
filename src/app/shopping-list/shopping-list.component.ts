3f5a78b8543801c0ec4d9858ca6cae5dc9d376a6
Wadih Ojeil <wadih.ojeil@gmail.com>
2020-08-17 12:07:44 -0400

    added the shopping cart form with buttons

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import{Ingredient} from '../shared/ingredient.model';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients=[];
+  ingredients: Ingredient[]=[
+    new Ingredient('Beef', 2),
+    new Ingredient('Cream', 1),
+  ];
 
   constructor() { }
 
