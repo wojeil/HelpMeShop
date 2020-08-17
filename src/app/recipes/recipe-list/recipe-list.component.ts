7123552b34cbecda2ff2b436809d22c04e813095
Wadih Ojeil <wadih.ojeil@gmail.com>
2020-08-17 11:38:09 -0400

    added structure to recipe detail component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,6 +8,7 @@ import {Recipe} from '../recipe.model'
 export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] =[
+    new Recipe('Test', 'Simply test', 'https://i2.wp.com/www.foodrepublic.com/wp-content/uploads/2012/05/testkitchen_argentinesteak.jpg?resize=1280%2C%20560&ssl=1'),
     new Recipe('Test', 'Simply test', 'https://i2.wp.com/www.foodrepublic.com/wp-content/uploads/2012/05/testkitchen_argentinesteak.jpg?resize=1280%2C%20560&ssl=1')
   ];
 
