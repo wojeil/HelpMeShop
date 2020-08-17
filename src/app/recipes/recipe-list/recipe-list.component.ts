e5558dbd30c7d4d9fa6d086a9d6baa914f31baf3
Wadih Ojeil <wadih.ojeil@gmail.com>
2020-08-17 11:09:22 -0400

    set up the recipe list about to work on a loop to display all my list items rather than one

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Recipe} from '../recipe.model'
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] =[
+    new Recipe('Test', 'Simply test', 'https://i2.wp.com/www.foodrepublic.com/wp-content/uploads/2012/05/testkitchen_argentinesteak.jpg?resize=1280%2C%20560&ssl=1')
+  ];
 
   constructor() { }
 
